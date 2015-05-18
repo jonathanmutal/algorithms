b16720e49d7e4ad133c71f7f0a5d8fd149dd80cf
jonathanmutal95 <jonathanmutal95@gmail.com>
2015-05-18 19:35:22 -0300

    s

--- discreta/API.c
+++ discreta/API.c
@@ -1,40 +1,99 @@
 #include "API.h"
 
-
-struct vertice{
-  uint32_t nombre;
-  uint32_t color;
-  struct vertice *vecinos;
+struct GrafSt {
+  u32 *list_ady_vert;
+  u32 *orden;
+  u32 *tabla_trad;
+  u32 *coloreo;
+  u32 *grado;
+  u32 colores_usados;
+  u32 n;
+  u32 m;
 };
 
-typedef struct vertice *vertice_t;
-
-struct GrafSt{
-  vertice_t vertices;
-};
-
-
-GrafP NuevoGraf(void){
+GrafP NuevoGraf(void) {
   return NULL;
 }
 
 int LeerGrafo(GrafP G){
   char c;
   bool done = false;
-  uint32_t n;
-  uint32_t m;
+  u32 n, m;
+  u32 izq, der;
+  u32 trad_izq, trad_der;
+  u32 tam = 0;
+
+  u32 *tabla_aux;
 
   while(!done){
     c = getc(stdin);
 
     if(c == 'p') {
       char edge[4];
-      fscanf(stdin, "%s" "%"PRIu32 "%"PRIu32, edge, &n, &m);
+      fscanf(stdin, "%s" "%u" "%u", edge, &n, &m);
       G = calloc(1, sizeof(struct GrafSt));
-      G->vertices = malloc(n * sizeof(struct vertice));
+      G->n = n;
+      G->m = m;
 
     } else if(c == 'e'){
-      fscanf(stdin, "%"PRIu32 "%"PRIu32, &n, &m);
+      fscanf(stdin, "%u" "%u", &izq, &der);
+
+
+      tabla_aux = calloc(n + 1, sizeof(u32));
+
+      G->list_ady_vert = calloc(n + 1, sizeof(u32));
+      G->orden = calloc(n + 1, sizeof(u32));
+      G->coloreo = calloc(n + 1 ,sizeof(u32));
+      G->tabla_trad = calloc(n + 1, sizeof(u32));
+      G->grado = calloc(n + 1, sizeof(u32));
+      G->colores_usados = 0;
+
+
+      for(u32 i = 1; i <= n; i++) {
+        tabla_aux[i] = 30;
+      }
+
+      for(u32 i = 1; i <= n; i++) {
+        G->list_ady_vert[i] = calloc(30, sizeof(u32));
+      }
+
+      for(u32 i = 1; i <= tam; i++) {
+        if(G->tabla_trad[i] == izq) {
+            trad_izq = i;
+            break;
+        }
+
+        if(trad_izq == 0) {
+          G->tabla_trad[tam + 1] = izq;
+          trad_izq = tam + 1;
+          tam = tam + 1;   
+        }
+
+        if(tabla_aux[trad_izq] == G->grado[trad_izq]) {
+           realloc(G->list_ady_vert[trad_izq], 30 * sizeof(u32));
+        }
+        G->grado[trad_izq] += 1;
+        G->list_ady_vert[trad_izq][G->grado[trad_izq]] = trad_der;
+      }
+
+      for(u32 i = 1; i <= tam; i++) {
+        if(G->tabla_trad[i] == der) {
+            trad_der = i;
+            break;
+        }
+
+        if(trad_der == 0) {
+          G->tabla_trad[tam + 1] = der;
+          trad_der = tam + 1;
+          tam = tam + 1;   
+        }
+
+        if(tabla_aux[trad_der] == G->grado[trad_der]) {
+          realloc(G->list_ady_vert[trad_der], 30 * sizeof(u32));/*ver bien*/
+        }
+        G->grado[trad_izq] += 1;
+        G->list_ady_vert[trad_der][G->grado[trad_der]] = trad_izq;
+      }
 
     } else if(c == 'c'){
       char linea[80];
@@ -43,6 +102,20 @@ int LeerGrafo(GrafP G){
     } else if(c == -1){
       done = true;
     }
+
   }
   return n;
+}
+
+int ImprimeGrafo(GrafP G) {
+    printf("p edge %u %u\n", G->n, G->m);
+
+    for(u32 i = 1; i <= G->n; i++) {
+        for (u32 j = 0 ; j < G->grado[i]; j++) {
+            if(i < G->list_ady_vert[i][j]) {
+                printf("e %u %u\n", G->tabla_trad[i], G->tabla_trad[G->list_ady_vert[i][j]]);
+            }
+        }
+    }
+    return 1;
 }
--- discreta/API.h
+++ discreta/API.h
@@ -1,17 +1,26 @@
-#ifndef API_H
-#define API_H
+#ifndef _API_H
+#define _API_H
 
-#include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include <stdio.h>
 #include <stdlib.h>
+
 
 typedef struct GrafSt *GrafP;
 
+typedef unsigned int u32;
+
 GrafP NuevoGraf(void);
+
+int DestruirGraf(GrafP G);
 
 int LeerGrafo(GrafP G);
 
+int ImprimeGrafo(GrafP G);
+
+u32 NumeroVerticesDeColor(GrafP G, u32 i);
+
+u32 ImprimirColor(GrafP G, u32 i);
+
+u32 CantidadDeColores(GrafP G);
 #endif
