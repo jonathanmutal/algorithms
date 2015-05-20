6431891e900b8bd8a53f5991883d978df096267a
jonathanmutal95 <jonathanmutal95@gmail.com>
2015-05-19 23:31:16 -0300

    Arreglo de todos los errores

--- discreta/API.c
+++ discreta/API.c
@@ -12,55 +12,52 @@ struct GrafSt {
 };
 
 GrafP NuevoGraf(void) {
-  return NULL;
+  GrafP GP = calloc(1, sizeof(struct GrafSt));
+  return GP;
 }
 
 int LeerGrafo(GrafP G){
-  char linea[80];
   char c;
-  bool done = false;
+  char edge[5];
+  char linea[80];
   u32 n, m;
-  u32 izq, der;
-  u32 trad_izq, trad_der;
+  u32 trad_der, trad_izq;
+  u32 izq,der;
   u32 tam = 0;
 
-  u32 *tabla_aux;
+  while((c = getc(stdin)) == 'c'){
+    fgets(linea, 80, stdin);
+  }
 
-  while(!done){
-    c = getc(stdin);
+  fscanf(stdin, "%s" "%u" "%u", edge, &n, &m);
 
-    if(c == 'p') {
-      char edge[4];
+  G->n = n;
+  G->m = m;
+  G->list_ady_vert = calloc(n, sizeof(u32*));
+  G->orden = calloc(n + 1, sizeof(u32));
+  G->coloreo = calloc(n + 1,sizeof(u32));
+  G->tabla_trad = calloc(n + 1, sizeof(u32));
+  G->grado = calloc(n + 1, sizeof(u32));
+  G->colores_usados = 0;
 
-      fscanf(stdin, "%s" "%u" "%u", edge, &n, &m);
-      fgets(linea, 80, stdin);
+  u32 *tabla_aux = calloc(n + 1, sizeof(unsigned int));
 
-      tabla_aux = calloc(n + 1, sizeof(u32));
+  for(u32 i = 1; i <= G->n; i++) {
+      tabla_aux[i] = 30;
+      G->list_ady_vert[i] = calloc(30, sizeof(u32));
+  }
 
-      G = calloc(1, sizeof(struct GrafSt));
-      G->n = n;
-      G->m = m;
-      G->list_ady_vert = calloc(n + 1, sizeof(u32 *));
-      G->orden = calloc(n + 1, sizeof(u32));
-      G->coloreo = calloc(n + 1 ,sizeof(u32));
-      G->tabla_trad = calloc(n + 1, sizeof(u32));
-      G->grado = calloc(n + 1, sizeof(u32));
-      G->colores_usados = 0;
+  for(u32 i = 1; i <= n; i++) {
+      tabla_aux[i] = 30;
+  }
 
-      for(u32 i = 1; i <= G->n; i++) {
-        tabla_aux[i] = 30;
-      }
+  while((c = getc(stdin)) != -1){
 
-      for(u32 i = 1; i <= G->n; i++) {
-        G->list_ady_vert[i] = calloc(30, sizeof(u32));
-      }
-
-    } else if(c == 'e'){
+    if (c == 'e'){
       fscanf(stdin, "%u" "%u", &izq, &der);
-      fgets(linea, 80, stdin);
 
-      trad_izq = 0;
       trad_der = 0;
+      trad_izq = 0;
 
       for(u32 i = 1; i <= tam; i++) {
         if(G->tabla_trad[i] == izq) {
@@ -69,59 +66,50 @@ int LeerGrafo(GrafP G){
         }
       }
 
+      if(trad_izq == 0) {
+        G->tabla_trad[tam + 1] = izq;
+        trad_izq = tam + 1;
+        tam = tam + 1;   
+      }
 
       for(u32 i = 1; i <= tam; i++) {
         if(G->tabla_trad[i] == der) {
-            trad_der = i;
-            break;
+          trad_der = i;
+          break;
         }
       }
 
       if(trad_der == 0) {
-        G->tabla_trad[tam + 1] = der;
-        trad_der = tam + 1;
-        tam = tam + 1;   
+          G->tabla_trad[tam + 1] = der;
+          trad_der = tam + 1;
+          tam = tam + 1;   
       }
 
-      if(tabla_aux[trad_der] == G->grado[trad_der]) {
-        tabla_aux[trad_der] += 30;
-        G->list_ady_vert[trad_der] = realloc(G->list_ady_vert[trad_der], tabla_aux[trad_der] * sizeof(u32 *));
-      }
-      G->grado[trad_izq] += 1;
-      G->list_ady_vert[trad_der][G->grado[trad_der]] = trad_izq;
-
-      if(trad_izq == 0) {
-        G->tabla_trad[tam + 1] = izq;
-        trad_izq = tam + 1;
-        tam += 1;
-      }
 
       if(tabla_aux[trad_izq] == G->grado[trad_izq]) {
-        tabla_aux[trad_izq] += 30;
-        G->list_ady_vert[trad_izq] = realloc(G->list_ady_vert[trad_izq], tabla_aux[trad_izq] * sizeof(u32 *));
+         G->list_ady_vert[trad_izq] = realloc(G->list_ady_vert[trad_izq], 30 * sizeof(u32));
       }
-      G->grado[trad_izq] += 1;
       G->list_ady_vert[trad_izq][G->grado[trad_izq]] = trad_der;
+      G->grado[trad_izq] = G->grado[trad_izq] + 1;
 
-    } else if(c == 'c'){
-      fgets(linea, 80, stdin);
-
-    } else if (c == -1){
-      done = true;
+      if(tabla_aux[trad_der] == G->grado[trad_der]) {
+         G->list_ady_vert[trad_der] = realloc(G->list_ady_vert[trad_der], 30 * sizeof(u32));
+      }
+      G->list_ady_vert[trad_der][G->grado[trad_der]] = trad_izq;
+      G->grado[trad_izq] = G->grado[trad_der] + 1;
     }
-
   }
 
   free(tabla_aux);
-  printf("SALI DEL WHILE\n");
-  return 0;
+  tabla_aux = NULL;
+  return n;
 }
 
 int ImprimeGrafo(GrafP G) {
     printf("p edge %u %u\n", G->n, G->m);
 
     for(u32 i = 1; i <= G->n; i++) {
-        for (u32 j = 0 ; j < G->grado[i]; j++) {
+        for (u32 j = 0 ; j <= G->grado[i]; j++) {
             if(i < G->list_ady_vert[i][j]) {
                 printf("e %u %u\n", G->tabla_trad[i], G->tabla_trad[G->list_ady_vert[i][j]]);
             }
