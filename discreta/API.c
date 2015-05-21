0242ca2226da2c133b7a3a8d43a1717de0a4cbc6
Guillermo Incatasciato <incatasciato.g.l@gmail.com>
2015-05-20 21:05:59 -0300

    alo

--- discreta/API.c
+++ discreta/API.c
@@ -16,7 +16,18 @@ GrafP NuevoGraf(void) {
   return GP;
 }
 
-int LeerGrafo(GrafP G){
+int DestruirGraf(GrafP G) {
+    for(u32 i = 1; i <= G->n; i++) free(G->list_ady_vert[i]);
+    free(G->list_ady_vert);
+    free(G->orden);
+    free(G->tabla_trad);
+    free(G->coloreo);
+    free(G->grado);
+    free(G);
+    return 1;
+}
+
+int LeerGrafo(GrafP G) {
   char c;
   char edge[5];
   char linea[80];
@@ -38,13 +49,14 @@ int LeerGrafo(GrafP G){
   G->coloreo = calloc(n + 1,sizeof(u32));
   G->tabla_trad = calloc(n + 1, sizeof(u32));
   G->grado = calloc(n + 1, sizeof(u32));
-  G->colores_usados = 0;
+  G->colores_usados = n;
 
   u32 *tabla_aux = calloc(n + 1, sizeof(u32));
 
   for(u32 i = 1; i <= G->n; i++) {
       tabla_aux[i] = 30;
       G->list_ady_vert[i] = calloc(30, sizeof(u32));
+      G->coloreo[i] = i;
   }
 
   while((c = getc(stdin)) != -1){
@@ -97,9 +109,10 @@ int LeerGrafo(GrafP G){
       }
       G->list_ady_vert[trad_der][G->grado[trad_der]] = trad_izq;
       G->grado[trad_der] = G->grado[trad_der] + 1;
+
     }
   }
-
+    
   free(tabla_aux);
   tabla_aux = NULL;
   return n;
@@ -116,4 +129,42 @@ int ImprimeGrafo(GrafP G) {
         }
     }
     return 1;
+}
+
+u32 NumeroVerticesDeColor(GrafP G, u32 i) {
+    u32 ncolores = 0;
+    for(u32 j = 1; j <= G->n; j++) {
+        if(G->coloreo[j] == i){
+            ncolores += 1;            
+        }
+    }
+    return ncolores;
+}
+
+u32 ImprimirColor(GrafP G, u32 i) {
+    u32 tabla_aux[G->n];
+    u32 ncolores = 0;
+
+    for(u32 i = 1; i <= G->n; i++) {
+        if(G->coloreo[i] == i) {
+            tabla_aux[ncolores] = G->tabla_trad[i];
+            ncolores += 1;
+        }
+    }
+
+    if(ncolores > 0) {
+        printf("Vertices de Color %u: \n", i);
+        for(u32 i = 0; i < ncolores - 1; i++){
+            printf("%u, ", tabla_aux[i]);
+        }
+        printf("%u.\n", tabla_aux[ncolores - 1]);
+    } else {
+        printf("No hay vertices de color %u\n", i);
+    }
+
+    return ncolores;
+}
+
+u32 CantidadDeColores(GrafP G) {
+    return G->colores_usados;
 }
