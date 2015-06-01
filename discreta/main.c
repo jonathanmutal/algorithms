0dc2d461a81455e8c12e6e1665dbc951df04f46e
jonathanmutal95 <jonathanmutal95@gmail.com>
2015-05-31 23:32:45 -0300

    Comienzo de main

--- discreta/main.c
+++ discreta/main.c
@@ -2,11 +2,44 @@
 
 int main(void){
     GrafP G = NuevoGraf();
-    LeerGrafo(G);
-    printf("Grafo Cargado...\n");
+
+    u32 leerGraf = LeerGrafo(G);
+
+    if(leerGraf == 0) {
+        printf("X(G) = 1\n");
+        return 0;
+    }
+
+    u32 dsatur = DSATUR(G);
+
+    if(dsatur == 2) {
+        printf("Grafo Bipartito\n");
+        DestruirGraf(G);
+        return 0;
+    }
+
     OrdenWelshPowell(G);
-    printf("GREEDY: %u\n", Greedy(G));
-    printf("DSATUR: %u\n", DSATUR(G));
-//    ImprimeGrafo(G);
+    u32 ordenWelshPowell = Greedy(G);
+
+    printf("DSATUR: Colores usados: %u\n", dsatur);
+    printf("Welsh-Powell: Colores usados: %u\n", ordenWelshPowell);
+
+    if(ordenWelshPowell == 3 || dsatur == 3) {
+        printf("X(G) == 3\n");
+        DestruirGraf(G);
+        return 0;
+    }
+
+//    printf("Grafo Cargado...\n");
+//    printf("Dsatur %u\n", DSATUR(G));
+//    OrdenWelshPowell(G);
+//    printf("GREEDY OrdenWelshPowell: %u\n", Greedy(G));
+//    Revierte(G);
+//    printf("GREEDY Revierte: %u\n", Greedy(G));
+//    GrandeChico(G);
+//    printf("GREEDY GrandeChico: %u\n", Greedy(G));
+//    ChicoGrande(G);
+//    printf("Greedy ChicoGrande: %u\n", Greedy(G));
+//    DestruirGraf(G);
     return 0;
 }
